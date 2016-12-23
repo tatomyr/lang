1db33b383a345eef62b723faa3e116dc5c0f2b3b
tatomyr <andrew.tatomyr@gmail.com>
2016-12-23 18:30:03 +0200

    cookies->localstorage

--- lang.js
+++ lang.js
@@ -8,7 +8,7 @@
 
 class Lang {
   constructor(defaultLanguage) {
-    this.set(this.getCookie('lang') || defaultLanguage || navigator.language.substr(0,2));
+    this.set(/* this.getCookie('lang') */ localStorage.lang || defaultLanguage || navigator.language.substr(0,2));
 
     $('[data-lang-set]').click((event) => {
       this.set(event.currentTarget.dataset.langSet);
@@ -16,43 +16,45 @@ class Lang {
   }
 
   set(lang) {
+    console.log('lang=->', lang);
     $(`[data-lang]`).addClass('lang-hidden');
     $(`[data-lang=${lang}]`).removeClass('lang-hidden');
 
     console.log('set lang:',lang);
 
-    this.setCookie('lang', lang);
+    // this.setCookie('lang', lang);
+    localStorage.setItem('lang', lang);
   }
 
-  setCookie(name, value, options) {
-    options = options || {};
-    var expires = options.expires;
-    if (typeof expires == "number" && expires) {
-      var d = new Date();
-      d.setTime(d.getTime() + expires * 1000);
-      expires = options.expires = d;
-    }
-    if (expires && expires.toUTCString) {
-      options.expires = expires.toUTCString();
-    }
-    value = encodeURIComponent(value);
-    var updatedCookie = name + "=" + value;
-    for (var propName in options) {
-      updatedCookie += "; " + propName;
-      var propValue = options[propName];
-      if (propValue !== true) {
-        updatedCookie += "=" + propValue;
-      }
-    }
-    document.cookie = updatedCookie;
-  }
-
-  getCookie(name) {
-    var matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-  }
+  // setCookie(name, value, options) {
+  //   options = options || {};
+  //   var expires = options.expires;
+  //   if (typeof expires == "number" && expires) {
+  //     var d = new Date();
+  //     d.setTime(d.getTime() + expires * 1000);
+  //     expires = options.expires = d;
+  //   }
+  //   if (expires && expires.toUTCString) {
+  //     options.expires = expires.toUTCString();
+  //   }
+  //   value = encodeURIComponent(value);
+  //   var updatedCookie = name + "=" + value;
+  //   for (var propName in options) {
+  //     updatedCookie += "; " + propName;
+  //     var propValue = options[propName];
+  //     if (propValue !== true) {
+  //       updatedCookie += "=" + propValue;
+  //     }
+  //   }
+  //   document.cookie = updatedCookie;
+  // }
+  //
+  // getCookie(name) {
+  //   var matches = document.cookie.match(new RegExp(
+  //     "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  //   ));
+  //   return matches ? decodeURIComponent(matches[1]) : undefined;
+  // }
 
 }
 
